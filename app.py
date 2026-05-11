75d546ecd02dd383cfc6973a858dc4d07a2267d4
Rakeo <rachelhuynh.1903@gmail.com>
2026-05-11 23:34:10 +0700

    Update landing page and search logic

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 from flask import Flask, render_template, request
-from logic.products import get_filtered_products
+from logic.products import get_paginated_products
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    keyword = request.args.get('search_box', '')
-    products_to_show = get_filtered_products(keyword)
-    return render_template('index.html', products=products_to_show, last_search=keyword)
+    keyword = request.args.get('search_box', '').strip()
+    page = request.args.get('page', 1, type=int)
+    data = get_paginated_products(query=keyword, page=page)
+    return render_template('index.html', **data, last_search=keyword)
 
 if __name__ == '__main__':
     app.run(debug=True)
